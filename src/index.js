b444e6e255d7b1c44168554854d7e4fd060d21be
david-trejo-armenta <a6518150041@utch.edu.mx>
2021-05-26 09:24:42 -0600

    Badge

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 import './index.css';
-import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 
 
 ReactDOM.render(

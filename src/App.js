04dcd41daa2d85f8aade64e225a041b23abd9eef
david-trejo-armenta <a6518150041@utch.edu.mx>
2021-05-26 10:02:12 -0600

    Header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Badge from './components/Badge'
 
 function App() {
   return (
-    <div>React App</div>
+    <Badge></Badge>
     );
 }
 
---
+++ src/components/Badge.js
@@ -0,0 +1,26 @@
+import React from "react";
+import HeaderBackground from '../images/background2.png'
+import ProfilePic from '../images/user.png'
+import "./style/Badge.css";
+
+class Badge extends React.Component {
+  render() {
+    return <React.Fragment>
+        <div className="Badge m-5 flex-column">
+            <div className="Badge__header">
+                <img src={HeaderBackground} alt="" />
+            </div>
+            <div className="Badge__userInfo container mt-5 mb-1">
+
+            </div>
+            <div className="Badge__info container pt-3">
+
+            </div>
+
+
+        </div>
+    </React.Fragment>;
+  }
+}
+
+export default Badge;

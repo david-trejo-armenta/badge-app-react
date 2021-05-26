11cff3d2d163188e02c6779d483800d26f0685df
david-trejo-armenta <a6518150041@utch.edu.mx>
2021-05-26 11:20:57 -0600

    react jsx

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -11,7 +11,11 @@ class Badge extends React.Component {
                 <img src={HeaderBackground} alt="Header Background" />
             </div>
             <div className="Badge__userInfo container mt-5 mb-1">
-
+              <div className="Badge__userImage">
+                <img src={ProfilePic} alt="Profile Picture"/>
+              </div>
+              <h4 className="text-center mt-5"> Isabella Fria</h4>
+              <p className="text-center"> Navo York City</p>
             </div>
             <div className="Badge__info container pt-3">
 

57961ef7e345fc77d0afc9fb6ea1bfe7ad9ae8d9
david-trejo-armenta <a6518150041@utch.edu.mx>
2021-05-26 12:35:20 -0600

    Badge Style

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import HeaderBackground from '../images/background2.png'
-import ProfilePic from '../images/user.png'
+import ProfilePic from '../images/user.jpg'
 import "./style/Badge.css";
 
 class Badge extends React.Component {
@@ -10,19 +10,37 @@ class Badge extends React.Component {
             <div className="Badge__header">
                 <img src={HeaderBackground} alt="Header Background" />
             </div>
-            <div className="Badge__userInfo container mt-5 mb-1">
+            <div className="Badge__userInfo container mt-5 mb-5">
               <div className="Badge__userImage">
                 <img src={ProfilePic} alt="Profile Picture"/>
               </div>
               <h4 className="text-center mt-5"> Isabella Fria</h4>
               <p className="text-center"> Navo York City</p>
             </div>
-            <div className="Badge__info container pt-3">
+              <div className="Badge__info container pt-3">
+              <div className="row">
+                <div className="col">
+                  <h4 className="text-center fw bold">80 k</h4>
+                  <p className="text-center">Followers</p>
+                </div>
+                <div className="col">
+                  <h4 className="text-center fw bold">803 k</h4>
+                  <p className="text-center">Likes</p>
+                </div>
+                <div className="col">
+                  <h4 className="text-center fw bold">80 </h4>
+                  <p className="text-center">Pictures</p>
+                </div>
+                
+                </div>
+                  </div>
+                  
+                
 
             </div>
 
 
-        </div>
+        
     </React.Fragment>;
   }
 }

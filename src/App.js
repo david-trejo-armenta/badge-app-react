711792868d6efb8d44f00bff5be28a0dd6e7bd57
david-trejo-armenta <a6518150041@utch.edu.mx>
2021-05-26 22:53:50 -0600

    Links created and applied to app.js source

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 import Layout from "./pages/Layout/Layout"
 import  Main from "./pages/main/Main"
+import  Error from "./pages/error/Error"
+import  SignIn from "./pages/signin/SignIn"
+import  Login from "./pages/login/Login"
+
+import NewBadge from "./pages/NewBadge/NewBadge"
+
+
+
 
 
 function App() {
   return (
-    <React.Fragment>
+<BrowserRouter>
       <Layout>
-        <Main title={"Welcome"}></Main>
+       <Switch>
+         <Route exact path = "/" component={Main}></Route>
+         <Route exact path = "/login" component={Login}></Route>
+         <Route exact path = "/SignIn" component={SignIn}></Route>
+         <Route exact path = "/new" component={NewBadge}></Route>
+         <Route component={Error}></Route>
+       </Switch>
       </Layout>
-    </React.Fragment>
     
+</BrowserRouter>      
     );
 }
 

cc2eef6a0c648971987f2e557813700b04995c1e
david-trejo-armenta <a6518150041@utch.edu.mx>
2021-05-26 18:05:57 -0600

    Dynamic Badge component

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -8,29 +8,29 @@ class Badge extends React.Component {
     return <React.Fragment>
         <div className="Badge m-5 flex-column">
             <div className="Badge__header">
-                <img src={HeaderBackground} alt="Header Background" />
+                <img src={this.props.header_picture} alt="Header Background" />
             </div>
             <div className="Badge__userInfo container mt-5 mb-5">
               <div className="Badge__userImage">
-                <img src={this.props.picture} alt="Profile Picture"/>
+                <img src={this.props.profile_picture} alt="Profile Picture"/>
               </div>
-              <h4 className="text-center mt-5"> Isabella Fria<i> 666</i></h4>
-              <p className="text-center"> Navo York City</p>
+              <h4 className="text-center mt-5"> {this.props.name}<i> {this.props.age}</i></h4>
+              <p className="text-center"> {this.props.city}</p>
             </div>
               <div className="Badge__info container pt-3">
               
               <div className="row">
                 <div className="col">
-                  <h4 className="text-center fw bold">666 k</h4>
+                  <h4 className="text-center fw bold">{this.props.followers}</h4>
                   <p className="text-center">Followers</p>
                 </div>
                 <div className="col">
-                  <h4 className="text-center fw bold">666 k</h4>
+                  <h4 className="text-center fw bold">{this.props.likes}</h4>
                   <p className="text-center">Likes</p>
                 </div>
                 <div className="col">
-                  <h4 className="text-center fw bold">666 </h4>
-                  <p className="text-center">Pictures</p>
+                  <h4 className="text-center fw bold">{this.props.post}</h4>
+                  <p className="text-center">Posts</p>
                 </div>
                 
                 </div>

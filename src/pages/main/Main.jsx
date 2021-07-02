0a07b1759d4118f5c79d0f9dacf1d58f65be830f
david-trejo-armenta <a6518150041@utch.edu.mx>
2021-07-01 21:18:17 -0600

    Main page update

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import "./Main.css"
 import Hero from "../../components/Hero"
+import Footer from "../../components/Footer"
 import Button from "../../components/MainButton"
 
 const Main = props =>{
@@ -9,11 +10,11 @@ const Main = props =>{
             <div className="Main">
             <div className="Main__content">
                 <h1 className="Main__title"> {props.title || "Welcome"}</h1>
-                <Button contentText={"Start"} link={"/new"}></Button>
+                <Button contentText={"Start"} theme={"Button"}link={"/badges"}></Button>
             </div>
             <Hero h={"88vh"}></Hero>
             </div>
-
+        <Footer s={{bottom:0}}></Footer>
         </React.Fragment>
     )
 }

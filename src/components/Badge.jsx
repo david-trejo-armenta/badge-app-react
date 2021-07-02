03214008132de427f8bd45411f374c6a6af5ace4
david-trejo-armenta <a6518150041@utch.edu.mx>
2021-07-01 21:22:50 -0600

    Badge info update

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -8,11 +8,11 @@ class Badge extends Component {
     <React.Fragment>
         <div className="Badge m-5 flex-column">
             <div className="Badge__header">
-                <img src={this.props.header_pic} alt="Header_Background" />
+                <img src={this.props.header_img_url} alt="Header_Background" />
             </div>
             <div className="Badge__userInfo container mt-5 mb-5">
               <div className="Badge__userImage">
-                <img src={this.props.profile_picture} alt="Profile_Picture"/>
+                <img src={this.props.profile_picture_url} alt="Profile_Picture"/>
               </div>
               <h4 className="text-center mt-5"> {this.props.name}<i>{this.props.age}</i></h4>
               <p className="text-center"> {this.props.city}</p>

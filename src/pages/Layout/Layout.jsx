9d653f53fb26b45bb7472de273693a8ffc556938
david-trejo-armenta <a6518150041@utch.edu.mx>
2021-07-01 21:26:41 -0600

    layout and new badge form update

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -9,7 +9,7 @@ function Layout(props){
             <div className="Layout">
             <Header></Header>
             {props.children}
-            <Footer></Footer>
+            
             </div>
         </React.Fragment>
     );
--- src/pages/NewBadge/NewBadge.jsx
+++ src/pages/NewBadge/NewBadge.jsx
@@ -2,8 +2,9 @@ import React from "react"
 import Hero from "../../components/Hero"
 import Badge from "../../components/Badge"
 import BadgeForm from "../../components/BadgeForm"
+import Footer from "../../components/Footer"
 import "./NewBadge.css"
-import api from "../../libs/api"
+import api from "../../libs/fetch"
 
 class NewBadge extends React.Component{
 
@@ -11,8 +12,8 @@ state = {
     loading : false,
     error: null,
     form:{
-        header_pic: "",
-        profile_picture: "",
+        header_img_url: "",
+        profile_picture_url: "",
         name: "",
         age: "",
         city:"",
@@ -38,7 +39,7 @@ handleSubmit = async (event) =>{
     try{
         await api.badges.create(this.state.form)
         this.setState({loading:false, error:null})
-        this.props.history.push("/")
+        this.props.history.push("/badges")
     }
     catch (error){
         this.setState({loading:false, error:error})
@@ -54,8 +55,8 @@ handleSubmit = async (event) =>{
              <div className="row">
                 <div className="col-6">
                 <Badge
-                header_pic = {this.state.form.header_pic || "https://images.pexels.com/photos/1072179/pexels-photo-1072179.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}
-                profile_picture = {this.state.form.profile_picture || "https://images.pexels.com/photos/162307/giant-tortoise-reptile-shell-walking-162307.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}
+                header_img_url = {this.state.form.header_img_url || "https://images.pexels.com/photos/1072179/pexels-photo-1072179.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}
+                profile_picture_url = {this.state.form.profile_picture_url || "https://images.pexels.com/photos/162307/giant-tortoise-reptile-shell-walking-162307.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}
                 name = {this.state.form.name || "Mariel Aguirre"}
                 age = {this.state.form.age || "21"}
                 city = {this.state.form.city || "Tokyo, Japan"}
@@ -73,6 +74,7 @@ handleSubmit = async (event) =>{
                 </div>
               </div>
             </div>
+            <Footer></Footer>
         </React.Fragment>
         );
     }

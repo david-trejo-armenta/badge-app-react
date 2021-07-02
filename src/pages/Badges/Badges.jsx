428fc01ab5e0ba43571f9ab4e9e593c57448bb4d
david-trejo-armenta <a6518150041@utch.edu.mx>
2021-07-01 21:13:19 -0600

    Badges information

---
+++ src/pages/Badges/Badges.jsx
@@ -0,0 +1,85 @@
+import React from "react"
+import SkeletonItem from "../../components/SkeletonItem"
+import BadgesList from "../../components/BadgesList"
+import Button from "../../components/MainButton"
+import api from "../../libs/fetch.js"
+import Footer from "../../components/Footer"
+import "./Badges.css"
+
+
+class Badges extends React.Component{
+
+    state ={
+        loading:true,
+        error: null,
+        data: undefined,
+        handle_footer: {bottom:0},
+    }
+
+    componentDidMount(){
+        this.fetchData();
+        this.setFetchInterval();
+    }
+
+    fetchData = async() =>{
+        this.setState({loading: true, error: null})
+        
+        try{
+
+        const data = await api.badges.list();
+            data.reverse()
+            this.setState({loading: false, data: data})
+
+            if(data.length > 3){
+                this.setState({handle_footer:{position: "relative"}})
+            }
+            else{
+                this.setState({handle_footer:{position:0}})
+            }
+        }
+        
+        catch(error){
+
+        this.setState({loading: false, error: error, data: []});
+
+        }
+    }
+
+        setFetchInterval(){
+            this.interval = setInterval(this.fetchData, 3000)
+        }
+
+        componentWillUnmount(){
+            clearInterval(this.interval)
+        }
+    
+
+    render() {
+        if(this.state.loading===true && !this.state.data){
+            return <SkeletonItem>
+                <h1>...........Skeleton...........</h1>
+            </SkeletonItem>
+        }
+        return(
+            <React.Fragment>
+                <h1>Friends</h1>
+                <div className="Badges__container">
+                    <div className="Badges__button">
+                        <Button 
+                        theme={"Button-light"}
+                        contentText = {"New Badge"}
+                        link={"/new"}
+                        >
+                        </Button>
+                    </div>
+                </div>
+                
+                <BadgesList badges={this.state.data}></BadgesList>
+                <Footer s={this.state.handle_footer}></Footer>
+                
+            </React.Fragment>
+        );
+    }
+}
+
+export default Badges
